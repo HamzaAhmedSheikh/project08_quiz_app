a6a4df52f240db52fec7f2d9740134de92d8edc7
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-02-10 01:23:36 +0500

    For the quiz app, I have made a welcome screen.

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+
+let playerName: string;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'Who Wants To Be A JavaScript Millionaire? \n'
+  );
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`
+    ${chalk.bgBlue('HOW TO PLAY')} 
+    I am a process on your computer.
+    If you get any question wrong I will be ${chalk.bgRed('killed')}
+    So get all the questions right...
+  `);
+}
+
+await welcome();
